182c5214822e006f6b562e2945511d3030d424bb
Nguyễn Hưng Khánh Tú <khanhtu261@gmail.com>
2025-05-23 10:04:48 +0700

    Update CustomCorsFilter.java

--- Dorm_Admin/src/main/java/cena/dorm_management/Dorm_Admin/configuration/CustomCorsFilter.java
+++ Dorm_Admin/src/main/java/cena/dorm_management/Dorm_Admin/configuration/CustomCorsFilter.java
@@ -19,8 +19,8 @@ public class CustomCorsFilter implements Filter {
 
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-
-	response.setHeader("Access-Control-Allow-Origin", "https://quanlyktx-3549.onrender.com");
+		    
+	response.setHeader("Access-Control-Allow-Origin", "https://dormitory-managerment-frontend.onrender.com");
 //      response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
 //	response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
